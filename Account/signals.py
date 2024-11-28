5cd435b7d4530b8b1b476b75fa2858ae0225ff24
Osmar <obazanalarcon@gmail.com>
2024-11-28 01:00:55 -0400

    AutoGeneracion de Info_ejercicio

--- Account/signals.py
+++ Account/signals.py
@@ -6,7 +6,7 @@ from django.core.exceptions import ObjectDoesNotExist
 
 @receiver(post_save, sender=User)
 def create_default_info_ejercicio(sender, instance, created, **kwargs):
-    if created and hasattr(instance, 'profile') and instance.profile.tipo_id == 1:
+    if created and hasattr(instance, 'profile') and instance.profile.tipo_id == 2:
         try:
             ejercicio = Ejercicio.objects.get(id=1)
             Info_Ejercicio.objects.get_or_create(
